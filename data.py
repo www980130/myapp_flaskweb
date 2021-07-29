1583711907ae1526807a8749a6539160ae0a6f2f
www980130 <www980130@naver.com>
2021-07-29 16:32:58 +0900

    Second Commit-게시판 및 클릭 시 게시 글 보여주기 기능 추가

---
+++ data.py
@@ -0,0 +1,39 @@
+def Boards():
+    boards = [  {  'index': 1,  'title':'Naturalized athletes aiming to fulfill Olympic dream in Tokyo',  'content':'''As the Tokyo Olympics\' motto "unity in diversity" says, there are five naturalized athletes across four sports who are competing for Korea in the 2020 Games.
+                 
+Out of 233 athletes representing Team Korea, only two percent are naturalized. Of the five naturalized athletes, two are part of the women\'s table tennis team and one each are competing in the men\'s marathon event, men\'s rugby sevens and women\'s basketball tournament.
+                 
+The "special naturalization" program was introduced in 2010, which allows qualified athletes to hold dual citizenship.
+                 
+Marathon runner Oh Joo-han (Wilson Loyanae Erupe), whose Korean name translates into "I run for Korea," is a strong medal contender in the Tokyo Olympics. Born in Kenya, Oh acquired Korean citizenship in August 2018.
+                 
+Hopes are high for Oh to win a medal at the running event, scheduled for Aug. 8, and provide a much-needed boost for Korea\'s marathon, which has stagnated in recent years.
+                 
+"My coach Oh Chang-seok, who unexpectedly passed away in May, always encouraged me to win an Olympic medal. I plan to do my best so I can dedicate my medal to him," Oh said in previous interviews.''',  'author':'Kwak Yeon-soo',  'create_date':'07-29-2021',  }, 
+ {  'index': 2,  'title':'Teenagers lead new mainstream for Korea',  'content':'''Korea\'s teenage Olympic athletes are the focus of keen attention, as a group of veteran medal hopefuls has ended up missing the opportunity to boost the nation\'s medal tally.
+  
+All the athletes creating a teenage sensation recently have been relatively little-known to the public, as this marks the first time that they have taken part in the quadrennial global sporting event.
+  
+But they have risen to global fame in the past week since the beginning of the Olympics, by mesmerizing fans here and abroad with their astonishing performances.
+  
+Archer Kim Je-deok has won two gold medals in Tokyo at the young age of 17. He brought home the first gold in the mixed team competition, and added one more gold during the men\'s team match. He sought his first triple crown, but ended up losing a match in the men\'s individual round of 16.
+  
+Kim started his archery career when he was 10, but he became known on the global stage only recently. He made his international debut in the 2021 Asia Cup, about a month before the beginning of the Olympics, and won the championship there.
+  
+Kim is widely expected to grow as the nation\'s star archer in upcoming global competitions, as has been shown from his outstanding results during the 2020 Summre Olympics.''',  'author':'Lee Min-hyung',  'create_date':'07-28-2021',  },  
+{  'index': 3,  'title': 'Economy, fairness to be key issues in presidential election', 'content': '''About seven months remain before the country chooses its new leader, and although it has yet to enter full-on election mode, dozens of contenders from both the ruling and opposition blocs have already announced their presidential bids.
+ 
+From the ruling Democratic Party of Korea (DPK), six contenders are in the running but the competition is now mainly about the rivalry between Gyeonggi Province Governor Lee Jae-myung, and former DPK Chairman and ex-Prime Minister Rep. Lee Nak-yon. The ruling party will select its final candidate in October.
+ 
+From the conservative opposition bloc, former Prosecutor General Yoon Seok-youl has long topped opinion polls of presidential hopefuls since his resignation from the top post in early March. Yoon has focused on criticizing the Moon Jae-in government\'s policy failures, and is expected to join the main opposition People Power Party (PPP) next month.
+ 
+The PPP has its own contenders, including Rep. Hong Joon-pyo and former four-term lawmaker Yoo Seong-min who ran in the 2017 election. Also, former Board of Audit and Inspection (BAI) Chairman Choe Jae-hyeong is rising as a strong contender, especially after joining the party, July 15, following his resignation from the top auditor post 17 days earlier. The PPP is expected to select its candidate in November.
+ 
+While the 2017 presidential election was all about replacing former President Park Geun-hye after she had been ousted by a major corruption scandal, and which brought about a power shift to the liberal bloc after 10 years of conservative rule, the 2022 election will be about the economy and fairness, according to political watchers. This means a contest on who can appeal more to the public in their promises to revive the coronavirus-hit economy in a fair manner.\n\n
+Solutions in post-coronavirus world.
+ 
+Political commentator Choi Young-il said economic indicators such as export volumes under the Moon administration show a relatively high performance compared to other countries in the global economy that has been hit hard by the COVID-19 pandemic; but people are not feeling any benefits from this in their daily lives.
+ 
+"The Moon government\'s pledges for fairness have not been realized in the economic sector," Choi said, citing failed policies on real estate market stabilization and job creation, which have only worsened wealth polarization.''',  'author':'Jung Da-min',  'create_date':'07-28-2021',  } ]
+    return boards
+
--- myapp.py
+++ myapp.py
@@ -1,11 +1,26 @@
-from flask import Flask # flask 모듈 import
+from flask import Flask, render_template, request # flask 모듈 import
+from data import Boards #모듈 형식으로 data.py의 articles 가져오기
 
 app = Flask(__name__) # __name__ 이라는 내장변수를 받아 새로운 instance인 app 생성
 app.debug = True # 파일 저장할 때마다 서버 restart 하기 위해 debug 설정 추가
 
+# root 페이지
 @app.route('/', methods = ['GET', 'POST']) # @ : decorate
 def main():
-    return "This is a main page"
+    return render_template("index.html")
+
+# board 페이지
+@app.route('/boards', methods=['GET', 'POST'])
+def boards():
+    board = Boards() # boards는 리스트 형태
+    return render_template("board.html", data = board)
+
+# 게시글별 글 내용 보여주는 페이지
+@app.route('/<index>/board', methods=['GET', 'POST'])
+def contents(index):
+    if request.method == "GET":
+        board = Boards()
+        return render_template("detail.html", detail = board[int(index)-1])
 
 if __name__ == '__main__':
     app.run(port = 3000)
